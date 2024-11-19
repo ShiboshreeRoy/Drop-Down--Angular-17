9a848598ff2d74d5a75d2b70d4001f8546eb5fed
ShiboshreeRoy <shiboshreeroy169@gmail.com>
2024-11-19 14:31:04 +0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,95 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+interface FormField {
+  type: string;
+  label: string;
+  control: any;
+  options?: string[];
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'DropDown';
+export class AppComponent implements OnInit {
+  formFields: any[] = [];
+  availableFields = [
+    { type: 'input', label: 'Number Input', control: new FormControl('') },
+    { type: 'select', label: 'Dropdown', control: new FormControl(''), options: [] },
+    { type: 'radio', label: 'Radio Button', control: new FormControl(''), options: [] },
+    { type: 'checkbox', label: 'Checkbox', control: new FormControl([]), options: [] }
+  ];
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({});
+  }
+
+  ngOnInit() {
+    //this.loadForm(); // Load the form when the component initializes
+  }
+
+  drop(event: CdkDragDrop<any[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      const newField = { ...event.previousContainer.data[event.previousIndex] };
+      this.formFields.push(newField);
+      this.form.addControl(`control-${this.formFields.length}`, newField.control);
+    }
+  }
+
+  updateLabel(fieldIndex: number, newLabel: string) {
+    this.formFields[fieldIndex].label = newLabel;
+  }
+
+  addOption(fieldIndex: number, newOption: string) {
+    this.formFields[fieldIndex].options.push(newOption);
+  }
+
+  removeOption(fieldIndex: number, optionIndex: number) {
+    this.formFields[fieldIndex].options.splice(optionIndex, 1);
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
+  }
+
+  saveForm() {
+    const formData = this.formFields.map(field => ({
+      type: field.type,
+      label: field.label,
+      options: field.options || []
+    }));
+    localStorage.setItem('savedForm', JSON.stringify(formData));
+  }
+
+  loadForm() {
+    const savedForm = localStorage.getItem('savedForm');
+    if (savedForm) {
+      this.formFields = JSON.parse(savedForm).map((field: any) => ({
+        ...field,
+        control: this.createControl(field)
+      }));
+      this.formFields.forEach((field, index) => {
+        this.form.addControl(`control-${index}`, field.control);
+      });
+    }
+  }
+
+  createControl(field: any): FormControl | FormGroup {
+    switch (field.type) {
+      case 'input':
+        return new FormControl('');
+      case 'select':
+        return new FormControl('');
+      case 'radio':
+        return new FormControl('');
+      case 'checkbox':
+        return new FormControl([]);
+      default:
+        return new FormControl('');
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatInputModule} from '@angular/material/input';
+import { MatSelectModule} from '@angular/material/select';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatCheckboxModule} from  '@angular/material/checkbox';
+import {ReactiveFormsModule,FormsModule} from '@angular/forms';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import {MatButtonModule} from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [
@@ -10,9 +17,19 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatCheckboxModule,
+    DragDropModule,
+    ReactiveFormsModule,
+    FormsModule,
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
